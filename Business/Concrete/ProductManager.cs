fa91a11cd041b11ad68a4b79e1318f4c21dd6964
rlBrky <rlbrky@users.noreply.github.com>
2021-07-15 20:30:46 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        //Bir adet IProductDal referansı alıyoruz, böylece herhangi bir referansta veritabanında çalışabileceğiz.
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            //İş kodları ve YetkiKontrolü
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Car> _cars;
+        public InMemoryProductDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=2016,DailyPrice=300,Description="1.6TSI Passat"},
+                new Car{Id=2,BrandId=1,ColorId=2,ModelYear=2020,DailyPrice=500,Description="1.6TDI Caddy"},
+                new Car{Id=3,BrandId=2,ColorId=3,ModelYear=2019,DailyPrice=1000,Description="A3 Sedan 1.6TDI"},
+                new Car{Id=4,BrandId=2,ColorId=1,ModelYear=2021,DailyPrice=1800,Description="A5 Sportback 40 TDI"},
+                new Car{Id=5,BrandId=2,ColorId=2,ModelYear=2015,DailyPrice=1450,Description="A4 Sedan Sport"},
+                new Car{Id=6,BrandId=3,ColorId=2,ModelYear=2021,DailyPrice=2500,Description="E 200D Exclusive"},
+
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToRemove = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToRemove);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.Where(c => c.Id == carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.Description = car.Description;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.ColorId = car.ColorId;
+        }
+    }
+}
